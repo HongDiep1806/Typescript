0f44044b3120d1712dd452cdfbbc172c729eb139
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-06-15 22:58:36 +0700

    types, annotations

---
+++ test/annotation.ts
@@ -0,0 +1,6 @@
+let  myfirstname: string = "Diep";
+let myAge: number = 21;
+let myHobbies: string[] = ["Cooking", "Reading"];    
+console.log("myfirstname = ", myfirstname);
+console.log("myAge = ", myAge);
+console.log("myHobbies = ", myHobbies);
---
+++ test/types.ts
@@ -0,0 +1,2 @@
+const firstName = 'Diep';
+console.log(">> check length = ", firstName.length)  
