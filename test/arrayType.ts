f76acf12d876bf2bb7bce678cb40019b2c3e04f5
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-06-24 23:06:54 +0700

    array Type

---
+++ test/arrayType.ts
@@ -0,0 +1,8 @@
+let array1: (number | string)[] = ['Diep', 22];
+let array2: string[] = ['Diep','Typescript'];
+let array3 = ['Diep', 22];
+array1.push('typescript');
+array1.push(18);
+console.log(">>> check array1: ", array1)
+console.log(">>> check array2: ", array2)
+console.log(">>> check array3: ", array3)
